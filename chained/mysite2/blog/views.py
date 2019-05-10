78d4066e54d4ed23096971f2515fce3357f59ae0
AuJaCef <45008462+AuJaCef@users.noreply.github.com>
2019-05-10 13:08:06 -0400

    Add files via upload

---
+++ chained/mysite2/blog/views.py
@@ -0,0 +1,12 @@
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+# Create your views here.
